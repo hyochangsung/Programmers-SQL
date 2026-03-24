febf5eeb3ec8074ef7bc015c4138a7a8cbd2090f
Hyo Chang <xx1733@naver.com>
2026-03-24 09:18:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,14 @@
+def solution(num):
+    cnt = 0
+    while True:
+        if num == 1:
+            break
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+        cnt += 1
+    if cnt > 500:
+        return -1
+    else:
+        return cnt
