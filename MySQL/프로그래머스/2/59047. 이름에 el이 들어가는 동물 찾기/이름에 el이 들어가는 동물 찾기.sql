93d7c115c2934432ad699acf63af56e19d1a9fb4
Hyo Chang <xx1733@naver.com>
2026-04-02 13:08:43 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,7 @@
+-- 할머니가 기르던 개는 이름에 'el'이 들어간다고 합니다.
+-- 동물 보호소에 들어온 동물 이름 중, 이름에 "EL"이 들어가는 개의 아이디와 이름을 조회
+-- 이때 결과는 이름 순으로 조회해주세요. 단, 이름의 대소문자는 구분하지 않습니다.
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = "DOG" AND NAME LIKE "%EL%"
+ORDER BY NAME ASC
